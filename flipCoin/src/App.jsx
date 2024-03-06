0b09f3daffb7a1aff33abd31f5867f79507035bd
MrChrisFabian <christopher.mendoza@fiuni.edu.py>
2024-03-06 14:14:53 -0300

    Tirar moneda listo

---
+++ flipCoin/src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+
+function App() {
+  const [intentos, setIntentos] = useState(0);
+  const [resultAct, setResultAct] = useState('')
+  const tossCoin = () => {
+    return Math.random() > 0.5 ? "cara" : "cruz";
+  }
+
+  const fiveCara = () => {
+    return new Promise((resolve, reject) => {
+
+      let contCara = 0;
+      let contIntento = 0;
+
+      while (contCara < 5) {
+        contIntento++;
+        let result = tossCoin();
+        console.log(result)
+        if (result === 'cara') {
+          contCara++;
+        }
+        else {
+          contCara = 0;
+        }
+      }
+      resolve(contIntento);
+    });
+  };
+
+  return (
+    <>
+      <button onClick={() => {
+        fiveCara().then(resp => setIntentos(resp))
+      }}>click</button>
+      <p>5 resultados de cara de seguido tomaron {intentos} intentos</p>
+    </>
+  )
+}
+
+export default App
